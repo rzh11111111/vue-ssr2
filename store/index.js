b6a6178c8f23df613e2d4cb1aaaa5cb8d6df9b2e
rzh11111111 <294346639@qq.com>
2020-05-17 19:41:26 +0800

    nuxtServerInit

--- store/index.js
+++ store/index.js
@@ -1,8 +1,12 @@
 // 1.定义到index
 // 2.实现nuxtServerInit
+// 在服务端跑
 export const actions = {
     nuxtServerInit({ commit }, { app }) {
-      // app是nuxt实例？koa实例？
+      // app是nuxt实例
+
+
+      //安装了cookie-universal-nuxt才能使用$cookies
       const token = app.$cookies.get("token");
       if (token) {
         console.log("nuxtServerInit: token:"+token);
